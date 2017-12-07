138b674029fb83f6b5b4b4361d23c1379697d088
Daniel D <leftpolaright@gmail.com>
2017-12-07 22:45:30 +0100

    Make valid api call on the address when available

--- client/eventListeners/findTransportsListener.js
+++ client/eventListeners/findTransportsListener.js
@@ -1,9 +1,21 @@
 import fetchApiKey from '../fetchers/fetchApiKey';
+import { castObject } from '../casts/castObject';
+import axios from 'axios';
 
 const findTransportsListener = (findTransportElement, addressElement) => {
   findTransportElement.addEventListener('click', (event) => {
     const address = addressElement.value;
-    console.log(address);
+    const key = fetchApiKey();
+    const geoLocationApiPath = "https://maps.googleapis.com/maps/api/geocode/json";
+    const geoLocationParams = {
+      address,
+      key
+    };
+    const geoLocationUrlParams = castObject(geoLocationParams).toUrlParams();
+    const apiCallPath = [geoLocationApiPath, '?', geoLocationUrlParams].join('');
+    axios.get(apiCallPath).then(response => {
+      console.log(response);
+    });
   });
 }
 
--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 
 export default {
     input: 'client/main.js',
@@ -7,8 +9,13 @@ export default {
         file: 'public/bundle.js',
         format: 'iife'
     },
+    sourceMap: 'inline',
     plugins: [
-        resolve(),
-        babel({ exclude: 'node_modules/**' })
+        resolve({ browser: true }),
+        babel({ exclude: 'node_modules/**' }),
+        replace({
+          'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        commonjs(),
     ],
 };
