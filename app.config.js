8c168fe000a723ead8cd53cc59a5a69a356b52c5
Daniel D <leftpolaright@gmail.com>
2017-12-12 22:51:29 +0100

    Setup the import action in the server

--- app.config.js
+++ app.config.js
@@ -1,3 +1,11 @@
 export default {
     gmaps_api_key: 'YOUR_KEY_HERE',
+    transit_api_key: 'YOUR_KEY_HERE',
+    mongoUrl: "mongodb://localhost:27017/gtfs",
+    agencies: [
+      {
+        agency_key: "a-name",
+        url: "https://api.transitfeeds.com/v1/getLatestFeedVersion?key=<YOUR_KEY_HERE>&feed=<THE_WANTED_FEED_HERE>"
+      }
+    ]
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 import express from 'express';
 import path from 'path';
+import gtfs from 'gtfs';
+import config from '../app.config';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUrl, { useMongoClient: true });
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve('public/index.html')));
+
+app.get('/import', (req, res) => {
+  gtfs.import(config)
+    .then(() => {
+      console.log("Import was successful!");
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+  res.send('Import request was submitted successfully!');
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
