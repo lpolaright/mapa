46cc9a52c0cc3315b6a7079845f9678806e7dccc
Daniel D <leftpolaright@gmail.com>
2017-12-13 22:19:54 +0100

    Start with getting the stops around point A

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import path from 'path';
 import gtfs from 'gtfs';
 import config from '../app.config';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import R from 'ramda';
 
 const app = express();
 
@@ -10,6 +12,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUrl, { useMongoClient: true });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.sendFile(path.resolve('public/index.html')));
 
@@ -23,6 +26,19 @@ app.get('/import', (req, res) => {
     });
 
   res.send('Import request was submitted successfully!');
+});
+
+app.post('/ideal-zones', (req, res) => {
+  const locA = R.view(R.lensPath(['locations', 0]))(req.body);  
+  const locB = R.view(R.lensPath(['locations', 1]))(req.body);
+  console.log(locB);
+  gtfs.getStops({ within: locA })
+    .then(stops => {
+      res.status(200).json(stops);
+    })
+    .catch(err => {
+      res.status(200).json(err);
+    });
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
